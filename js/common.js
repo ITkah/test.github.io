9e0e31e81ecc31cad467e0958e4187a6d52516d4
ITkah <ilatkacenko132@gmail.com>
2019-12-22 15:11:36 +0200

    bug done

--- js/common.js
+++ js/common.js
@@ -12,12 +12,17 @@ $(document).ready(function() {
 
     $(".menu-btn").on("click", function(){
         $(".hide-top-menu").toggleClass("show-hide-menu");
-        $("body").toggleClass("over-hide");
+        $(".dark-bg").show();
     });
 
     $(".close-but").on("click", function(){
         $(".hide-top-menu").removeClass("show-hide-menu");
-        $("body").removeClass("over-hide");
+        $(".dark-bg").hide();
+    });
+
+    $(".dark-bg").on("click", function(){
+        $(".hide-top-menu").removeClass("show-hide-menu");
+        $(".dark-bg").hide();
     });
 
     $('.main-slider .owl-carousel').owlCarousel({
@@ -149,7 +154,7 @@ $(document).ready(function() {
         $(this).toggleClass("arrow-active");
     }
 
-    $('#accordeon .acc-head').on('click', f_acc);
+    $('.accordeon-catogory .acc-head').on('click', f_acc);
     
     $(".add-wish").on("click", function(){
         $(this).toggleClass("active-heart");
@@ -159,4 +164,79 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).toggleClass("add-cart-active");
     });
+
+    $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+    });
+
+    $(".modal-feedback-form").submit(function (e) {
+        e.preventDefault();
+
+        var form_data = $(this).serializeArray();
+
+        $.ajax({
+            type: "POST",
+            url: "/php/mail.php",
+            data: form_data,
+            success: function () {
+                $(".modal-feedback .mfp-close").click();
+                $(".call-thank").click();
+            },
+            error: function (error) {
+                console.log(error);
+                //Удалить нижние 2 строчки
+                $(".modal-feedback .mfp-close").click();
+                $(".call-thank").click();
+            }
+        });
+
+        return false;
+    });
+
+    $('.minus-btn').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $input = $this.closest('div').find('input');
+        var value = parseInt($input.val());
+     
+        if (value > 1) {
+            value = value - 1;
+        } else {
+            value = 0;
+        }
+     
+      $input.val(value);
+     
+    });
+     
+    $('.plus-btn').on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $input = $this.closest('div').find('input');
+        var value = parseInt($input.val());
+     
+        if (value < 100) {
+            value = value + 1;
+        } else {
+            value = 100;
+        }
+     
+        $input.val(value);
+    });
+
 });
+
